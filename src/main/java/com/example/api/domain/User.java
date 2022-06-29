9453f0dc5ea76f369cae85eaef0dab6471555008
Wallace82 <gomesw@gmail.com>
2022-06-29 11:51:43 -0300

    classe user resource findById

--- src/main/java/com/example/api/domain/User.java
+++ src/main/java/com/example/api/domain/User.java
@@ -2,23 +2,23 @@ package com.example.api.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 
 @Data
-@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    @Column(unique = true)
+
+   // @Column(unique = true)
     private String email;
     private String password;
 }
