f071ba4822458fab254b30fb1cea5466ee0288c5
Wallace82 <gomesw@gmail.com>
2022-06-29 10:24:30 -0300

    Initial commit

---
+++ src/main/java/com/example/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
